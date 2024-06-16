88cea64cf1c338cd1c0254d535724ee9653285be
MohitParashar06 <162456215+MohitParashar06@users.noreply.github.com>
2024-06-16 12:14:11 +0530

    Update formModel.js

--- backend/models/formModel.js
+++ backend/models/formModel.js
@@ -1,6 +1,15 @@
 const db = require('./db');
 
 db.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  db.query("CREATE DATABASE mydb", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+  });
+});
+
+db.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
     var sql = "CREATE TABLE forms (formType VARCHAR(20), name VARCHAR(20), countryCode VARCHAR(20), phoneNumber VARCHAR(20))";
